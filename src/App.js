b7bdbd74077103505efb25bc5a2b1a7241e806a9
Khansaabyousafxai <abdullahshah707172@gmail.com>
2024-03-03 17:57:27 +0500

    my-first react-app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Hi I AM ABDULLAH SHAH
+        </p>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
